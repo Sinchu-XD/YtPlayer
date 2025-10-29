0b50af1d7d1077f0b13459e9622895e67a450739
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-10-29 22:32:51 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -27,4 +27,4 @@ async def search(query: str = Query(..., min_length=1)):
         return {"status": False, "error": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
